0c9e686505452873eb962be9afd0787bb7e1e205
ShabiGardezi <shabigardezi51214@gmail.com>
2024-11-02 04:49:45 +0500

    children-story

--- api/routes/watsonRoutes.js
+++ api/routes/watsonRoutes.js
@@ -3,6 +3,7 @@ const WatsonController = require("../controllers/watsonController");
 const validateArabicInputMiddleware = require("../middleware/validateArabicInputMiddleware");
 const validateArabicProofReadingMiddleware = require("../middleware/validateArabicProofReadingMiddleware");
 const validateArabicGrammarAnalysisMiddleware = require("../middleware/validateArabicGrammarAnalysisMiddleware");
+const validateChildrenStoryInputMiddleware = require("../middleware/validateChildrenStoryInputMiddleware");
 
 const router = express.Router();
 
@@ -22,7 +23,11 @@ router.post(
   validateArabicGrammarAnalysisMiddleware,
   WatsonController.generateGrammaticalAnalysis
 );
-router.post("/children-story", WatsonController.generateChildrenStory);
+router.post(
+  "/children-story",
+  validateChildrenStoryInputMiddleware,
+  WatsonController.generateChildrenStory
+);
 router.post("/marketing-text", WatsonController.generateMarketing);
 
 module.exports = router;
